a2ef0577841dbd7dc714caac4e1c3d6bf5df0fbd
Antonio Isaac Ramirez Monsalve <metal_invaders_21@hotmail.com>
2022-04-25 17:48:50 -0400

    Input&FormField

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
-  styleUrls: ['./shared.component.scss']
+  styleUrls: ['./shared.component.scss'],
 })
 export class SharedComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  isInline!: boolean;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      input: [
+        null,
+        {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.minLength(3)],
+        },
+      ],
+    });
   }
 
+  onPatchValue(): void {
+    this.form.patchValue({ input: 'Antonio ramirez' });
+  }
+  onSubmit(): void {
+    console.log('presiono boton submit');
+  }
+  organizarElemento(){
+    this.isInline = !this.isInline;
+  }
 }
