4f001c0af2dfd56786bfe7cb41acaf225fb36e82
Antonio Isaac Ramirez Monsalve <metal_invaders_21@hotmail.com>
2022-04-24 21:35:52 -0400

    estilosDemoComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from 'environments/environment';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
